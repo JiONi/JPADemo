307947b3cfabfe558d6b7f211f8f655bc86a0a1e
JiONi <uphp179@gmail.com>
2019-07-29 22:48:16 +0900

    새로운 어노테이션 매핑을 활용한 컬럼 추가, 스키마 자동 생성 기능 속성 추가

--- src/main/java/jpabook/start/Member.java
+++ src/main/java/jpabook/start/Member.java
@@ -1,6 +1,7 @@
 package jpabook.start;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name="MEMBER")
@@ -15,6 +16,19 @@ public class Member {
 
     //매핑 정보가 없는 필드
     private Integer age;
+
+    @Enumerated(EnumType.STRING) // java enum을 사용해서 회원 타입 구분. enum을 사용하기 위해 @Enumerated 사용
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)   // 자바의 날짜 타입은 @Temporal을 사용해서 매핑한다.
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob    // @Lob 어노테이션을 사용하면 CLOB, BLOB 타입을 매핑할 수 있다.
+    private String description;
+
 
     //Getter, Setter
     public String getId(){
@@ -38,3 +52,4 @@ public class Member {
         this.age = age;
     }
 }
+
